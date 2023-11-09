f7330ab57d3e0057ea88867975c51a5e3017825b
theBigBengi <imthebigbengi@gmail.com>
2023-11-09 15:04:41 +0200

    analytics2

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 export function Analytics() {
-  const token = process.env.NEXT_PUBLIC_BEAM_TOKEN;
-  if (!token) {
-    return null;
-  }
+  //   const token = process.env.NEXT_PUBLIC_BEAM_TOKEN;
+  //   if (!token) {
+  //     return null;
+  //   }
   return (
     <script
       src='https://beamanalytics.b-cdn.net/beam.min.js'
